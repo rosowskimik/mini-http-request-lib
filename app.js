9fd86b98d671af380e12c52a5a3b4ec95120a50a
rosowskimik <m.rosowski1@wp.pl>
2019-05-21 16:42:56 +0200

    Mini http request library finished

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const http = new easyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/posts1', function(err, posts) {
-	if(err) {
-		console.log(err);
-	} else {
-		console.log(posts);
-	}
-})
+const data = {
+	title: 'Test post',
+	body: 'This is test post'
+};
+
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(posts);
+// 	}
+// })
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+// });
+
+// http.put('https://jsonplaceholder.typicode.com/posts/37', data, function(err, post) {
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+// });
+
+// http.delete('https://jsonplaceholder.typicode.com/posts/37', function(err, response) {
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(response);
+// 	}
+// });
